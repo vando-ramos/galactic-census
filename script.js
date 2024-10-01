b68714eb43edf692bcb9e887c3afd91c923b3bba
vando <vandorramos@gmail.com>
2024-09-30 23:29:54 -0300

    Create a button to display each planet retrieved from the API.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let list = document.getElementById('list');
+
 async function printData() {
+
   let res = await fetch('https://swapi.dev/api/planets/?format=json');
-  let data = await res.json();
-  console.log(data.results);
+
+  let { results } = await res.json();
+
+  console.log(results);
+
+  results.forEach(planet => {
+
+    let li = document.createElement('li');
+
+    li.innerHTML = `<div>                      
+                      <button>${planet.name}</button>
+                    </div>`;
+
+    list.appendChild(li);
+  });     
 }
 
 printData();
